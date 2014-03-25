ab661ef350a56fc0b633822f5193d3c89dcd333a
git status <rogesson>
2014-03-25 01:21:46 +0000

    Criacao do user controller

--- Proto/config/routes.rb
+++ Proto/config/routes.rb
@@ -1,5 +1,7 @@
 Proto::Application.routes.draw do
   resources :main
+
+  resources :user
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
