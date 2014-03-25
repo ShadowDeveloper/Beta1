eb5530856b20be2c6e502d19cd3f45c2b3f56db9
git status <rogesson>
2014-03-25 02:02:45 +0000

    Criacao do sistema de login com validacao de senha em banco

--- Proto/app/controllers/user_controller.rb
+++ Proto/app/controllers/user_controller.rb
@@ -1,6 +1,11 @@
 class UserController < ApplicationController
 	def create
-
+		if User.where( login: params[:user_name], password: params[:password]) != []
+			response = "/user"
+		else
+			response = "/main?password=1"
+		end
+		redirect_to response
 	end
 
 	def index
