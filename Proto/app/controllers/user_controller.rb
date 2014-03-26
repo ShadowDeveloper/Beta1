30f6ee186892216e7fac1b361a73e9295aaac621
Rodrigo <ro_web1@hotmail.com>
2014-03-25 21:43:57 -0300

    Correcao algoritmo de login

--- Proto/app/controllers/user_controller.rb
+++ Proto/app/controllers/user_controller.rb
@@ -9,21 +9,19 @@ class UserController < ApplicationController
 
 	def login
 		user = User.where(login: params[:user_name])
-		pass = User.where(password: params[:password])
 		if user != []
-			session[:user_id] = user[0].id
-			session[:user_login] = user[0].login
-			response = "/user/#{user[0].id}"
+			if user[0].password == params[:password]
+				session[:user_id] = user[0].id
+				session[:user_login] = user[0].login
+				response = "/user/#{user[0].id}"
+			else
+				response = "/main?password=1"
+			end
+			
 		else
 			response = "/main?erruser=1"
 		end
-
-		if pass != []
-			session[:pass_id] = pass[0].id
-			session[:pass_login] = pass[0].login
-		else
-			response = "/main?password=1"
-		end
+			
 
 		redirect_to response
 	end
