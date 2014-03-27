48b4bd223bcaef31460cad8a3359c4c35f7f2504
git status <rogesson>
2014-03-27 02:13:32 +0000

    criacao do BE de cadastro de clientes

--- Proto/app/controllers/client_controller.rb
+++ Proto/app/controllers/client_controller.rb
@@ -1,6 +1,16 @@
 class ClientController < ApplicationController
 	def create
-		
+		Client.create(
+			:cpf => params[:cpf],
+			:name => params[:name],
+			:dob => params[:dob],
+			:rg => params[:rg],
+			:phone => params[:phone],
+			:cep => params[:cep],
+			:number => params[:number]
+		)
+
+		redirect_to :back
 	end
 	def index
 		
