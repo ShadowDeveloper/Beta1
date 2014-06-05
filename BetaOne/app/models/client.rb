925f0c2d3c738bfa55a4db63b7675362961ca582
git status <rogesson>
2014-06-05 02:09:33 +0100

    alterando a ordem de listagem de clientes

--- BetaOne/app/models/client.rb
+++ BetaOne/app/models/client.rb
@@ -1,6 +1,8 @@
 class Client < ActiveRecord::Base
 	self.table_name = "client"
 	 
+	default_scope order "id desc"
+	
 	validates :cpf, uniqueness: { message: " - CPF existente na base de dados." }
 
 	has_many :client_references
