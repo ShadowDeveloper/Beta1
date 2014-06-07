8b6601e588cacc1cfeb64c5e1e5e6f892abd4c5f
git status <rogesson>
2014-06-07 02:39:54 +0100

    correcao dos testes de usuario

--- BetaOne/test/controllers/user_controller_test.rb
+++ BetaOne/test/controllers/user_controller_test.rb
@@ -6,75 +6,56 @@ class UserControllerTest < ActionController::TestCase
 		session[:user_id] = 3
 	end
 
-   	test "the truth" do
-    	assert true
-   	end
+	test "the truth" do
+ 	assert true
+	end
 
-   	test "should create a new simple user" do 
-   		params = {
-   			login: "rsouza",
-   			name:  "rogesson",
-   			cpf:   "129312873"
-   		}
+	test "should create a new simple user" do 
+		params = {
+			login: "rsouza",
+			name:  "rogesson",
+			cpf:   "129312873"
+		}
 
-   		post(:create, params)
-   		assert_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
-   	end
+		post(:create, params)
+		assert_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
+	end
 
-   	test "should create a new ADMIN user" do 
-   		params = {
-   			login: "cbarboza",
-   			name:  "carlos barboza",
-   			cpf:   "129312873",
-   			admin: 1
-   		}
+	test "should create a new ADMIN user" do 
+		params = {
+			login: "cbarboza",
+			name:  "carlos barboza",
+			cpf:   "121121211",
+			admin: 1
+		}
 
-   		post(:create, params)
-   		assert_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
-   	end
+		post(:create, params)
+		assert_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
+	end
 
-   	test "should not create user with duplicate cpf" do
+	test "should not create user with duplicate cpf" do
+		params = {
+			login: "lsilva",
+			name:  "lucas silva",
+			cpf:   "#{User.first.cpf}",
+			admin: 1
+		}
+		post(:create, params)
+		
+		assert_not_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
+		
+	end
 
-   		params1 = {
-   			login: "lsilva",
-   			name:  "lucas silva",
-   			cpf:   "129312873",
-   			admin: 1
-   		}
-   		post(:create, params1)
-
-   		params2 = {
-   			login: "rcarlos",
-   			name:  "lucas silva",
-   			cpf:   "129312873",
-   			admin: 1
-   		}
-   		post(:create, params1)
-   		
-   		assert_equal User.count, 6
-   		
-   	end
-
-   		
-   	test "should not create user with duplicate login" do
-
-   		params1 = {
-   			login: "rcarlos",
-   			name:  "lucas silva",
-   			cpf:   "221231222",
-   			admin: 1
-   		}
-   		post(:create, params1)
-
-   		params2 = {
-   			login: "rcarlos",
-   			name:  "lucas silva",
-   			cpf:   "129312873",
-   			admin: 1
-   		}
-   		post(:create, params1)
-   		
-   		assert_equal User.count, 6
-   		
-   	end
+		
+	test "should not create user with duplicate login" do
+		params = {
+			login: "#{User.first.login}",
+			name:  "lucas silva",
+			cpf:   "2112121222",
+			admin: 1
+		}
+		post(:create, params)
+		
+		assert_not_equal(response.body, "$('#status')[0].innerHTML='<center>REGISTRO CRIADO COM SUCESSO!<center>'")
+	end
 end
