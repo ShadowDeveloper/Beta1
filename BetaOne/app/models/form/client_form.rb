c9d8a23b7197722d6de77b001963acfab6486693
rogesson <rogessonb@gmail.com>
2014-08-08 02:13:25 +0100

    criacao de forms

---
+++ BetaOne/app/models/form/client_form.rb
@@ -0,0 +1,4 @@
+class Form::ClientForm
+	attr_acessor :name, :rg, :dob, :cpf, :relationship, :address, :phone_number, :phone_number2, :email_address,
+	:email_address2, :mobile_number, :mobile_number2, :client_type
+end
