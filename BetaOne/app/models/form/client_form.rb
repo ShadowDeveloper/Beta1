69385011f178021184ab75189c3f45307be039ba
rogesson <rogessonb@gmail.com>
2014-08-08 02:30:54 +0100

    atributos do form

--- BetaOne/app/models/form/client_form.rb
+++ BetaOne/app/models/form/client_form.rb
@@ -1,4 +1,11 @@
 class Form::ClientForm
+	include ActiveModel::Model
+
 	attr_acessor :name, :rg, :dob, :cpf, :relationship, :address, :phone_number, :phone_number2, :email_address,
 	:email_address2, :mobile_number, :mobile_number2, :client_type
+
+	validates :name,  presence: true
+	validates :rg,    presence: true
+	validates :dob,   presence: true
+	
 end
