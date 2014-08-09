2e027eb22e7653a0607d167f464deb08fc442bec
rogesson <rogessonb@gmail.com>
2014-08-09 03:39:36 +0100

    alterando params

--- BetaOne/app/models/form/client_form.rb
+++ BetaOne/app/models/form/client_form.rb
@@ -2,22 +2,19 @@ class Form::ClientForm
 	include ActiveModel::Model
 
 	attr_accessor :name, :rg, :dob, :cpf, :relationship, :address, :phone_number, :phone_number2, :email_address,
-	:email_address2, :mobile_number, :mobile_number2, :client_type
+	:email_address2, :mobile_number, :mobile_number2, :client_type, :income, :extra_income, :ocupation,
+	:company_name,  :name, :phone_number, :phone_number2, :bank_id, :agency, :account_number
+	
 
-	validates :name,  presence: true
-	validates :rg,    presence: true
-	validates :dob,   presence: true
-	validates :cpf, presence: true    
-	validates :relationship, presence: true
-	validates :address, presence: true
-	validates :phone_number, presence: true
-	validates :phone_number2, presence: true
-	validates :email_address, presence: true
-	validates :email_address2, presence: true
-	validates :mobile_number, presence: true
-	validates :mobile_number2, presence: true
-	validates :client_type, presence: true
+	validates :name, :rg, :dob, :cpf, :relationship, :address, :phone_number, :phone_number2, :email_address,
+	:email_address2, :mobile_number, :mobile_number2, :client_type, presence: true
 
+	validates :income, :extra_income, :ocupation, :company_name, presence: true
+
+	validates :name, :phone_number, :phone_number2, presence: true
+
+	validates :bank_id, :agency, :account_number, presence: true
+	
 	def validate
 		valid? ? true : false
 	end
