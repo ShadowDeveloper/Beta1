0d7a3e812aac5084afd94d8743c97f8b0bf2eabd
rogesson <rogessonb@gmail.com>
2014-08-23 00:34:27 +0000

    criacao do submenu

--- BetaOne/app/helpers/application_helper.rb
+++ BetaOne/app/helpers/application_helper.rb
@@ -3,10 +3,9 @@ module ApplicationHelper
         user = User.find(user_id)
       
         arr = Array.new
-        nav_itemx = arr.push({id: "home", href: "/users/<%= session[:user_id]%>", value: "Home"})
-        nav_itemx = arr.push({id: "clients", href: "/clients/new", value: "Cadastrar Cliente"})
-        nav_itemx = arr.push({id: "users", href: "/users/new", value: "Cadastrar Usuario"})
-        nav_itemx = arr.push({id: "clients/list", href: "/clients/list", value: "Buscar Cliente"})
+        nav_itemx = arr.push({id: "home", href: "#", value: "Home"})
+        nav_itemx = arr.push({id: "clients", href: "/clients/new", value: "Cliente"})
+        nav_itemx = arr.push({id: "users", href: "/users/new", value: "Usuario"})
     
         nav_itemx
     end
