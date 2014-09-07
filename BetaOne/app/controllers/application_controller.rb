f208874182a89022695461b654e42757972d5f2e
rogesson <rogessonb@gmail.com>
2014-09-07 15:25:14 +0100

    alteracao no controle de sessao

--- BetaOne/app/controllers/application_controller.rb
+++ BetaOne/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
    	def verify_active_session
-	  	if session[:user_id] == nil
+	  	if session[:user_id].nil?
 	  		redirect_to "/main"
 	  	end
   	end
