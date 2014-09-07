9869fbcce7c86793260d003f015222f7ba0f8a01
rogesson <rogessonb@gmail.com>
2014-09-07 15:51:07 +0100

    utf 8 + residencia no menu

--- BetaOne/app/helpers/application_helper.rb
+++ BetaOne/app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module ApplicationHelper
     def self.nav_item user_id
         user = User.find(user_id)
@@ -5,7 +6,8 @@ module ApplicationHelper
         arr = Array.new
         nav_itemx = arr.push({id: "home", href: "/home", value: "Home"})
         nav_itemx = arr.push({id: "clients", href: "/clients/list", value: "Cliente"})
-        nav_itemx = arr.push({id: "users", href: "/users/list", value: "Usuario"})
+        nav_itemx = arr.push({id: "users", href: "/users/list", value: "Usuário"})
+        nav_itemx = arr.push({id: "residences", href: "/residences/list", value: "Imóveis"})
     
         nav_itemx
     end
