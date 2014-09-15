255e2ec2f0602c516ee4238729446f05f2994ee6
rogesson <rogessonb@gmail.com>
2014-09-15 14:42:56 +0000

    integracao da tabela de usuario

--- BetaOne/app/models/client.rb
+++ BetaOne/app/models/client.rb
@@ -1,5 +1,21 @@
 class Client < ActiveRecord::Base
-	self.table_name = "client"
+	self.table_name = "cliente"
+
+    alias_attribute :id, :id_cliente
+    alias_attribute :client_type, :ctipo_cliente
+    alias_attribute :name, :cnome
+    alias_attribute :cpf,  :ccpf
+    alias_attribute :marital_status, :cestado_civil
+    alias_attribute :cep, :ccep
+    alias_attribute :street, :clogradouro
+    alias_attribute :addrress, :cendereco
+    alias_attribute :house_nuber, :cnumero
+    alias_attribute :house_n_complement:, :ccomplemento
+    alias_attribute :city, :ccidade
+    alias_attribute :neighborhood, :cbairro
+    alias_attribute :nationality, :cnacionalidade
+    alias_attribute :profession, :cprofissao
+    alias_attribute :created_at, :ddta_Cadastro
 
 	default_scope { order :id => :desc }
 	
@@ -7,10 +23,10 @@ class Client < ActiveRecord::Base
 	
 	validates :cpf, uniqueness: { message: " - CPF existente na base de dados." }
 	
-	has_many :client_references
+	#has_many :client_references
 
-	has_one :company
+	#has_one :company
 
-	has_many :bank_account
+	#has_many :bank_account
 
 end
