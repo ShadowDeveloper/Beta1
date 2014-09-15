586d164154d916f1cd1b2aab74c5c65340864122
rogesson <rogessonb@gmail.com>
2014-09-15 14:50:22 +0000

    migracao da tabela de usuario

--- BetaOne/app/models/residence.rb
+++ BetaOne/app/models/residence.rb
@@ -1,3 +1,25 @@
 class Residence < ActiveRecord::Base
 	self.table_name = "residence"
 end
+
+=begin
+CREATE TABLE imovel(
+id_imovel auto_increment primary key,
+id_client int,
+c_status char(1),
+c_tipo char(1),
+ccep varcha(10),
+clogradouro varchar(10),
+cendereco varchar(80),
+cnumero varchar(8),
+ccomplemento varchar(20),
+ccidade varchar(50),
+cbairro varchar(30),
+cestado char(2),
+czona char(1),
+cvistoria char(1),
+ddatcadastro datetime,
+iicdastradopor int,
+nmetragem numeric
+);
+=end
--- BetaOne/app/models/user.rb
+++ BetaOne/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
-	self.table_name = "user"
-
+	self.table_name = "usuario"
+    
+    alias_attribute :id, :id_usuario
+    alias_attribute :cresci_id, :id_cresci
+    alias_attribute :name, :cnome
+    alias_attribute :cpf, :ccpf
+    alias_attribute :phone_number
+    
 	validates :cpf, uniqueness: {message: " - CPF existente na base de dados."}
 	validates :login, uniqueness: {message: " - Login existente na base de dados."}
 	validates :name, presence: true
