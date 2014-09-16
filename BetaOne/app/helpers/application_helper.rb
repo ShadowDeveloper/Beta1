3053c9ad33c9a082ba3d0bfcdb68b59cc6a44e90
shadowdeveloper <ro_web1@hotmail.com>
2014-09-16 16:15:31 -0300

    cor da barra esquerda e icones

--- BetaOne/app/helpers/application_helper.rb
+++ BetaOne/app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     def self.nav_item
       
         arr = Array.new
-        nav_itemx = arr.push({id: /home/, href: "/home", value: "Home", icon:"fi-home"})
+        nav_itemx = arr.push({id: /home/, href: "/home", value: "Home", icon:"fi-folder"})
         nav_itemx = arr.push({id: /clients/, href: "/clients/list", value: "Cliente", icon:"fi-torso"})
         nav_itemx = arr.push({id: /users/, href: "/users/list", value: "Usuário", icon:"fi-torso-business"})
         nav_itemx = arr.push({id: /residences/, href: "/residences", value: "Imóveis", icon:"fi-home"})
