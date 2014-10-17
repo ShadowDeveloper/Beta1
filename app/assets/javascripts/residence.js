0bcfa199e3aa7f27698514cd2e41afc4cdfc3d38
rogesson <rogessonb@gmail.com>
2014-10-17 04:46:19 +0100

    hide function

--- app/assets/javascripts/residence.js
+++ app/assets/javascripts/residence.js
@@ -1,3 +1,7 @@
-function hideOptionButton(id){
+function showOptionButton(id){
 	$("#btn_group-residence-"+id).css("display","block")
+}
+
+function hideOptionButton(id){
+	$("#btn_group-residence-"+id).css("display","none")
 }
