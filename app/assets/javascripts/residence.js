844ff10e0043e5f951a76e038eacc6191ed77a32
rogesson <rogessonb@gmail.com>
2014-10-27 19:08:22 -0200

    validacao do form de imoveis

--- app/assets/javascripts/residence.js
+++ app/assets/javascripts/residence.js
@@ -34,3 +34,78 @@ function validateFormSearchResidence(){
     }
   );  
 }
+
+//funcao de validacao de campos da busca de imovel
+function validateFormNewResidence(){
+  console.log('validateFormNewResidence')
+ 
+  $('form').submit(
+    function(){
+      var form_ok = true
+
+      if ( !validate($("#residence_info_area_total")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_qtd_garage")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_qtd_banheiro")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_qtd_suite")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_qdt_domitorio")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_condominium_value")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_rent_value")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_sell_value")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_info_residence_code")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_number")) ){
+        form_ok = false;
+      }
+
+
+      if ( !validate($("#residence_cep")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_residence_type")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#residence_status")) ){
+        form_ok = false;
+      }
+
+      if ( !validate($("#cpf")) ){
+        form_ok = false;
+      }
+
+      if (form_ok){
+        true;
+      } else {
+        event.preventDefault();
+        fnDefaultMessage('Preecha todos os campos obrigatórios')
+      }
+    }
+  );  
+}
