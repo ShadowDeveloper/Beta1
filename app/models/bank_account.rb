a00dade09dcfd12296f65d8cd391ec5ef6022adf
rogesson <rogessonb@gmail.com>
2014-11-01 05:40:54 +0000

    ajuste no banco

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,5 +1,23 @@
+# encoding: utf-8
+
 class BankAccount < ActiveRecord::Base
 	self.table_name = "bank_account"
 	
 	belongs_to :client
+
+	def bank_name
+		bank_id = self.bank_id
+
+		 if bank_id == 1
+		 	"Itaú"
+		 elsif bank_id == 2
+		 	"Bradesco"
+		 elsif bank_id == 3
+		 	"Santander"
+		 elsif bank_id == 4
+		 	"Caixa"
+		 else
+         "Caixa"
+		 end
+	end
 end
