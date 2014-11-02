fe2746ff1540e176e6ffdb21035ddd567e0922fe
rogesson <rogessonb@gmail.com>
2014-11-02 18:46:24 +0000

    upload de img completo

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ActiveRecord::Base
+
+	belongs_to :residence
 	has_attached_file :pics, style: { thumb: '120x120', large: '300x400' }
 	attr_accessor :pics_file_name
 end
--- app/models/residence.rb
+++ app/models/residence.rb
@@ -2,12 +2,15 @@ class Residence < ActiveRecord::Base
 	self.table_name = "residences"
 	validates :cep, presence: true
 
-	has_one :residence_info, dependent: :destroy
+	has_one 	  :residence_info, dependent: :destroy
 	belongs_to :client
-
-	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_many   :picture
+	has_attached_file :pics, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   	validates_attachment_content_type :pics, :content_type => /\Aimage\/.*\Z/
+
+  	
 	
+
 	def position
 		position = Geocoder.search(self.street_code)[0].data["geometry"]["location"]
 		{
